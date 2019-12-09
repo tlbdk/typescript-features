966cb350711a2bfe19da0f59e670882487956ab5
Troels Liebe Bentsen <tlb@nversion.dk>
2019-12-10 00:35:40 +0100

    Add ordered array

--- src/async-array.test.ts
+++ src/async-array.test.ts
@@ -1,16 +1,13 @@
-import { AsyncArray } from './async-array'
+import { AsyncArray, AsyncOrderedArray } from './async-array'
 
 describe('AsyncArray', () => {
-  it('should iterate of array of promises with error', async () => {
-    const array = new AsyncArray<Promise<number>>(
+  it('should iterate over array of promises with error', async () => {
+    const array = new AsyncArray(
+      0,
       Promise.resolve(1),
       Promise.resolve(2),
       // Make sure we go to next tick
-      new Promise(resolve =>
-        setTimeout(() => {
-          resolve(3)
-        }, 1)
-      ),
+      new Promise(resolve => setTimeout(resolve, 1, 3)),
       Promise.reject(new Error('4'))
     )
 
@@ -23,7 +20,24 @@ describe('AsyncArray', () => {
     } catch (e) {
       error = e
     }
-    expect(count).toEqual(3)
+    expect(count).toEqual(4)
     expect(error).toEqual(new Error('4'))
+  })
+
+  it('should return items in order', async () => {
+    const array = new AsyncOrderedArray(
+      Promise.resolve(1),
+      0,
+      new Promise(resolve => setTimeout(resolve, 30, 4)),
+      new Promise(resolve => setTimeout(resolve, 10, 2))
+    )
+    array.push(new Promise(resolve => setTimeout(resolve, 20, 3)))
+
+    const items: Array<number> = []
+    for await (const item of array) {
+      items.push(item)
+    }
+
+    expect(items).toEqual([0, 1, 2, 3, 4])
   })
 })
--- src/async-array.ts
+++ src/async-array.ts
@@ -1,13 +1,22 @@
 import { isPromise } from './promise-utils'
 
-export class AsyncArray<T> extends Array<T> {
-  constructor(...items: T[]) {
+export class AsyncArray<T> extends Array<T | Promise<T>> {
+  constructor(...items: Array<T | Promise<T>>) {
     for (const item of items) {
       if (isPromise(item)) {
         item.catch(e => e)
       }
     }
     super(...items)
+  }
+
+  push(...items: Array<T | Promise<T>>): number {
+    for (const item of items) {
+      if (isPromise(item)) {
+        item.catch(e => e)
+      }
+    }
+    return super.push(...items)
   }
 
   [Symbol.asyncIterator](): AsyncIterator<T> {
@@ -28,5 +37,79 @@ export class AsyncArray<T> extends Array<T> {
         return { done: false, value: next.value }
       }
     }
+  }
+}
+
+type DeferredPromise<T> = {
+  resolve: (value?: T | PromiseLike<T>) => void
+  reject: (reason?: any) => void
+}
+
+// TODO: Does it make sense to extend from AsyncArray?
+export class AsyncOrderedArray<T> extends AsyncArray<T> {
+  private orderedDeferred: Array<DeferredPromise<T>>
+
+  constructor(...items: Array<T | Promise<T>>) {
+    const orderedDeferred: Array<DeferredPromise<T>> = []
+    const orderedPromises: Array<Promise<T>> = []
+    const simpleItems: Array<T> = []
+    for (const item of items) {
+      if (isPromise(item)) {
+        orderedPromises.push(
+          new Promise<T>((resolve, reject) => {
+            orderedDeferred.push({ resolve, reject })
+          })
+        )
+        item
+          .then(res => {
+            const promise = orderedDeferred.shift()
+            if (promise) {
+              promise.resolve(res)
+            }
+          })
+          .catch(e => {
+            const promise = orderedDeferred.shift()
+            if (promise) {
+              promise.reject(e)
+            }
+          })
+      } else {
+        simpleItems.push(item)
+      }
+    }
+    super(...simpleItems, ...orderedPromises)
+    this.orderedDeferred = orderedDeferred
+  }
+
+  push(...items: Array<T | Promise<T>>): number {
+    const orderedPromises: Array<Promise<T>> = []
+    for (const item of items) {
+      orderedPromises.push(
+        new Promise<T>((resolve, reject) => {
+          this.orderedDeferred.push({ resolve, reject })
+        })
+      )
+      if (isPromise(item)) {
+        item
+          .then(res => {
+            const promise = this.orderedDeferred.shift()
+            if (promise) {
+              promise.resolve(res)
+            }
+          })
+          .catch(e => {
+            const promise = this.orderedDeferred.shift()
+            if (promise) {
+              promise.reject(e)
+            }
+          })
+      } else {
+        const promise = this.orderedDeferred.shift()
+        if (promise) {
+          promise.resolve(item)
+        }
+      }
+    }
+    return super.push(...orderedPromises)
   }
 }
